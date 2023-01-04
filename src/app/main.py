4ba05d8c385c4eb0c27b4e6995ae8b9cfa11d61c
maxiaoteng <xiaoteng@bigonelab.com>
2023-01-04 17:21:04 +0800

    index

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from app.database import Base, engine
 from app.tools.router import router as tools_router
@@ -6,5 +9,13 @@ from app.tools.router import router as tools_router
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", context={"request": request})
+
 
 app.include_router(tools_router, prefix="/tools", tags=["Tools"])
